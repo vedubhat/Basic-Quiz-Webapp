5ddde0515da9ac6148d1c20ea3319aed6ea4dccb
Vedant <vedusbat9@gmail.com>
2024-07-23 20:17:32 +0530

    use list of object as data structure to store the questions instead of using arrays

--- script.js
+++ script.js
@@ -1,75 +1,80 @@
+const questions = [
+    {
+        question: "Q.Who invented Java Programming?",
+        options: ["a) Guido van Rossum", "b) James Gosling", "c) Vedant", "d) Dennis Ritchie"],
+        ansIndex: 1
+    },
+    {
+        question: "Q.Which statement is true about Java?",
+        options: ["a) Java is a sequence-dependent programming language", "b) Java is a code dependent programming language", "c) Java is a platform-dependent programming language", "d) Java is a platform-independent programming language"],
+        ansIndex: 3
+    },
+    {
+        question: "Q.Which component is used to compile, debug and execute the java programs",
+        options: ["a) JRE", "b) JIT", "c) JDK", "d) JVM"],
+        ansIndex: 2
+    },
+    {
+        question: "Q.Which of these cannot be used for a variable name in Java",
+        options: ["a) identifier & keyword", "b) identifier", "c) keyword", "d) none of the mentioned"],
+        ansIndex: 2
+    }
+]
+
 let question = document.getElementById("question");
-let answer = document.querySelectorAll(".answers")
-const ListofQues = [
-    "Q.Who invented Java Programming?",
-    "Q.Which statement is true about Java?",
-    "Q.Which component is used to compile, debug and execute the java programs",
-    "Q.Which of these cannot be used for a variable name in Java",
-    "Q.What is the extension of java code files?"
-];
-
-const ListofAns = [
-    ["a) Guido van Rossum","b) James Gosling","c) Vedant","d) Dennis Ritchie"],
-    ["a) Java is a sequence-dependent programming language","b) Java is a code dependent programming language","c) Java is a platform-dependent programming language","d) Java is a platform-independent programming language"],
-    ["a) JRE","b) JIT","c) JDK","d) JVM"],
-    ["a) identifier & keyword","b) identifier","c) keyword","d) none of the mentioned"],
-    ["a) .js","b) .txt","c) .class","d) .java"]
-];
+let answer = document.querySelectorAll(".answers");
+let button = document.getElementById("submit-btn");
 
 
-const CorrectAns = [
-    "b) James Gosling",
-    "d) Java is a platform-independent programming language",
-    "c) JDK",
-    "c) keyword",
-    "d) .java"
-];
+function start() {
+    question.innerText = "Do you want to start the quiz ?";
+    answer.innerText = "";
+    button.innerText = "Start";
+}
 
+start();
 // console.log(question.innerText);
 let count = 0;
 let points = 0;
-let q;
-    let button = document.getElementById("submit-btn");
-    button.addEventListener("click" ,() => {
-        count++;
-        if(count == 4){
-            alert("You have scored " + points)
-            count = 0;
-            points = 0;
-        }
-        document.getElementById("1").style.backgroundColor = "#8a2be2";
-        document.getElementById("2").style.backgroundColor = "#8a2be2";
-        document.getElementById("3").style.backgroundColor = "#8a2be2";
-        document.getElementById("4").style.backgroundColor = "#8a2be2";
+let ques;
 
+button.addEventListener("click", () => {
 
+    button.innerText = "Next"
+    document.getElementById("1").style.backgroundColor = "#8a2be2";
+    document.getElementById("2").style.backgroundColor = "#8a2be2";
+    document.getElementById("3").style.backgroundColor = "#8a2be2";
+    document.getElementById("4").style.backgroundColor = "#8a2be2";
 
-        let a = Math.random() * (4 - 0) + 0;
-        q = Math.floor(a);
-        question.innerText = ListofQues[q]
-        document.getElementById("1").innerText = ListofAns[q][0];
-        document.getElementById("2").innerText = ListofAns[q][1];
-        document.getElementById("3").innerText = ListofAns[q][2];
-        document.getElementById("4").innerText = ListofAns[q][3];
+    let a = Math.random() * (4 - 0) + 0;
+    ques = questions[Math.floor(a)];
+    question.innerText = ques.question
+    ques.options.forEach((option, idx) => {
+        document.getElementById(idx + 1).innerText = option
     })
 
+})
 
 
-    let answerBoxx = document.getElementById("answerBox")
-    answerBoxx.addEventListener("click", (e) => {
-        if(e.target.innerText === CorrectAns[q]){
-            (e.target.style.backgroundColor = "#00FF00")
-            points++;
-        }
-        else{
-            (e.target.style.backgroundColor = "red")
-            answer.forEach(element => {
-                if(element.innerText === CorrectAns[q]){
-                    (element.style.backgroundColor = "#00FF00")
-                }
-            });
-        }
-    })
+
+let answerBoxx = document.getElementById("answerBox")
+answerBoxx.addEventListener("click", (e) => {
+    const correctAns = ques.options[ques.ansIndex];
+    console.log("coloring",correctAns);
+    console.log(e.target.innerText);
+    if (e.target.innerText === correctAns) {
+        (e.target.style.backgroundColor = "#00FF00")
+        points++;
+    }
+    else {
+        (e.target.style.backgroundColor = "red")
+        answer.forEach(element => {
+            if (element.innerText === correctAns) {
+                (element.style.backgroundColor = "#00FF00")
+            }
+        });
+    }
+})
 
 
 
